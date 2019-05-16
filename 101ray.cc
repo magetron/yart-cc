de5f4e9d254c92570b227fece14e6634441b49e9
Patrick Wu <magetron1@gmail.com>
2019-05-17 00:53:16 +0800

    use ray class to produce 101 graph

---
+++ 101ray.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "ray.h"
+
+vec3 colour (const ray& r) {
+	vec3 unit_direction = unit_vector(r.direction());
+	double t = 0.5  * (unit_direction.y() + 1.0);
+	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
+
+int main () {
+
+	freopen("101ray.ppm", "w", stdout);
+
+	int nx = 2000;
+	int ny = 1000;
+
+	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+
+	vec3 lower_left_corner (-2.0, -1.0, -1.0);
+	vec3 horizontal (4.0, 0.0, 0.0);
+	vec3 vertical (0.0, 2.0, 0.0);
+	vec3 origin (0.0, 0.0, 0.0);
+
+	for (int j = ny - 1; j >= 0; j--) {
+		for (int i = 0; i < nx; i++) {
+			double u = double(i) / double(nx);
+			double v = double(j) / double(ny);
+			ray r (origin, lower_left_corner + u * horizontal + v * vertical);
+			vec3 col = colour(r);
+			int ir = int(255.99 * col[0]);
+			int ig = int(255.99 * col[1]);
+			int ib = int(255.99 * col[2]);
+
+			std::cout << ir << " " << ig << " " << ib << "\n";
+		}
+	}
+
+	fclose(stdout);
+	return 0;
+}
+
+
+
