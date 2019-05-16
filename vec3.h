2a7017fb0be3a506b67fec8c628947a070de40f4
Patrick Wu <magetron1@gmail.com>
2019-05-16 12:23:02 +0800

    add vec3 class

--- vec3.h
+++ vec3.h
@@ -34,3 +34,12 @@ class vec3 {
 			return sqrt (e[0] * e[0] + e[1] * e[1] + e[2] * e[2]); 
 		}
 
+		inline double squared_length() const {
+			return e[0] * e[0] + e[1] * e[1] + e[2] * e[2]; 
+		}
+
+		inline void make_unit_vector();
+
+		double e[3];
+};
+
