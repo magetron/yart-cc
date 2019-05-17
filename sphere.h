91a29113a16b03f881e4acdfe4233f570c56dfbc
Patrick Wu <magetron1@gmail.com>
2019-05-17 18:21:10 +0800

    add sphere class

---
+++ sphere.h
@@ -0,0 +1,35 @@
+#include "hittable.h"
+
+class sphere: public hittable {
+	public:
+		sphere () { }
+		sphere (vec3 cen, double r) : centre(cen), radius(r)	{ } ;
+		virtual bool hit (const ray& r, double tmin, double tmax, hit_record& rec) const;
+		vec3 centre;
+		double radius;
+};
+
+bool sphere::hit (const ray& r, double t_min, double t_max, hit_record& rec) const {
+	vec3 oc = r.origin() - center;
+	double a = dot(r.direction(), r.direction());
+	double b = dot(oc, r.direction());
+	double c = dot(oc, oc) - radius * radius;
+	double determinant = b * b - a * c; 		// should be b ^ 2 - 4 ac, however redundant two's are eliminated in future calculations
+	if (determinant > 0) {
+		double tmp = (- b - sqrt(determinant)) / a;
+		if ( (tmp < t_max) && (tmp > t_min) ) {
+			rec.t = tmp;
+			rec.p = r.point_at_parameter(rec.t);
+			rec.normal = (rec.p - centre) / radius;
+			return true;
+		}
+		tmp = (- b + sqrt(determinant)) / a;
+		if ( (tmp < t_max) && (tmp > t_min) ) {
+			rec.t = tmp;
+			rec.p = r.point_at_parameter(rec.t);
+			rec.normal = (rec.p - centre) / radius;
+			return true;
+		}
+	}
+	return false;
+}
