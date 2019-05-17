57f52aecccd1aa521f88150a8b30946e15ff681b
Patrick Wu <magetron1@gmail.com>
2019-05-17 11:01:36 +0800

    add abstract class for hittable objects

---
+++ hitable.h
@@ -0,0 +1,14 @@
+#include "ray.h"
+
+struct hit_record {
+	double t;
+	vec3 p;
+	vec3 nomral;
+}
+
+class hitable {
+	public:
+		virtual bool hit (const ray& r, double t_min, double t_max, hit_record& rec) const = 0;
+};
+
+
