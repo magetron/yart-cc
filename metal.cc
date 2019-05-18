755ea5a961a6d45b05c75da2ceca44c735569315
Patrick Wu <magetron1@gmail.com>
2019-05-18 13:21:20 +0800

    update metal renderer to render fuzzied metal material

--- metal.cc
+++ metal.cc
@@ -37,8 +37,8 @@ int main () {
 	hittable *list[4];
 	list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
 	list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
-	list[3] = new sphere(vec3(-1, 0, -1), 0.5, new metal(vec3(0.8, 0.8, 0.8)));
+	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0));
+	list[3] = new sphere(vec3(-1, 0, -1), 0.5, new metal(vec3(0.8, 0.8, 0.8), 1.0));
 	hittable *world = new hittable_list(list, 4);
 
 	for (int j = ny - 1; j >= 0; j--) 
