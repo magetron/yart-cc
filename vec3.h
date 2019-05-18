c3c1c55702b974db08f166d06442dbb552612632
Patrick Wu <magetron1@gmail.com>
2019-05-18 21:27:55 +0800

    add unary operator - for vec3

--- vec3.h
+++ vec3.h
@@ -62,6 +62,10 @@ inline vec3 operator + (const vec3 &v1, const vec3 &v2) {
 	return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
 }
 
+inline vec3 operator - (const vec3 &v) {
+	return vec3(-v.e[0], -v.e[1], -v.e[2]);
+}
+
 inline vec3 operator - (const vec3 &v1, const vec3 &v2) {
 	return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);
 }
