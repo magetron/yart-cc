b24876e8313edc8966359f0a674c8fc5c5d37aa4
Patrick Wu <magetron1@gmail.com>
2019-05-18 13:20:57 +0800

    add fuzziness for metal material

--- metal.h
+++ metal.h
@@ -2,6 +2,7 @@
 #define METALH
 
 #include "material.h"
+#include "lambertian.h"
 
 vec3 reflect (const vec3& v, const vec3& n) {
 	return v - 2 * dot(v, n) * n;
@@ -9,15 +10,16 @@ vec3 reflect (const vec3& v, const vec3& n) {
 
 class metal : public material {
 	public : 
-		metal (const vec3& a) : albedo(a) { }
+		metal (const vec3& a, double f) : albedo(a) { if (f < 1) fuzz = f; else fuzz = 1; }
 		virtual bool scatter (const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
 			vec3 reflected = reflect(unit_vector(r_in.direction()),  rec.normal);
-			scattered = ray(rec.p, reflected);
+			scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
 			attenuation = albedo;
 			return (dot(scattered.direction(), rec.normal) > 0);
 		}
 		
 		vec3 albedo;
+		double fuzz;
 };
 
 #endif
