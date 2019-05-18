6d8faa0948a02d89825cb6919027bd56cea2ddaf
Patrick Wu <magetron1@gmail.com>
2019-05-18 12:41:29 +0800

    add metal and lambertian material

---
+++ metal.h
@@ -0,0 +1,24 @@
+#ifndef METALH
+#define METALH
+
+#include "material.h"
+
+vec3 reflect (const vec3& v, const vec3& n) {
+	return v - 2 * dot(v, n) * n;
+}
+
+class metal : public material {
+	public : 
+		metal (const vec3& a) : albedo(a) { }
+		virtual bool scatter (const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+			vec3 reflected = reflect(unit_vector(r_in.direction()),  rec.normal);
+			scattered = ray(rec.p, reflected);
+			attenuation = albedo;
+			return (dot(scattered.direction(), rec.normal) > 0);
+		}
+		
+		vec3 albedo;
+};
+
+#endif
+
