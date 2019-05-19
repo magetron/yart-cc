7894dd987f12becff4f78165288bdc4d9507b0da
Patrick Wu <magetron1@gmail.com>
2019-05-19 13:19:22 +0800

    add adjustable FOV camera

--- camera.h
+++ camera.h
@@ -5,11 +5,16 @@
 
 class camera {
 	public:
-		camera () { 
-			lower_left_corner = vec3(-2.0, -1.0, -1.0);
-			horizontal = vec3(4.0, 0.0, 0.0);
-			vertical = vec3(0.0, 2.0, 0.0);
+		camera (double vfov, double aspect) {
+			double theta = vfov * M_PI / 180;
+			double half_height = tan(theta / 2);
+			double half_width = aspect * half_height;
+
+			lower_left_corner = vec3(-half_width, -half_height, -1.0);
+			horizontal = vec3(2 * half_width, 0.0, 0.0);
+			vertical = vec3(0.0, 2 * half_height, 0.0);
 			origin = vec3(0.0, 0.0, 0.0);
+		
 		}
 
 		ray get_ray (double u, double v) {
