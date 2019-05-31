a4f9420964ae62a7bca00f03647b6d051d445727
Patrick Wu <magetron1@gmail.com>
2019-05-31 14:16:48 +0800

    add box x,y,z cmp function

--- headers/bvh_node.h
+++ headers/bvh_node.h
@@ -27,6 +27,26 @@ int box_x_cmp (const void* a, const void* b) {
 	if (box_left.min().x() - box_right.min().x() < 0.0) return -1; else return 1;
 }
 
+int box_y_cmp (const void* a, const void* b) {
+	aabb box_left, box_right;
+	hittable *ah = *(hittable **) a;
+	hittable *bh = *(hittable **) b;
+
+	if (!ah -> bounding_box(0, 0, box_left) || !bh -> bounding_box(0, 0, box_right))
+		std:cerr << "no bounding box in bvh_node constructor\n";
+	if (box_left.min().y() - box_right.min().y() < 0.0) return -1; else return 1;
+}
+
+int box_z_cmp (const void* a, const void* b) {
+	aabb box_left, box_right;
+	hittable *ah = *(hittable **) a;
+	hittable *bh = *(hittable **) b;
+
+	if (!ah -> bounding_box(0, 0, box_left) || !bh -> bounding_box(0, 0, box_right))
+		std:cerr << "no bounding box in bvh_node constructor\n";
+	if (box_left.min().z() - box_right.min().z() < 0.0) return -1; else return 1;
+}
+
 
 bvh_node :: bvh_node (hittable **l, int n, double time0, double time1) {
 	int axis = int(3 * drand48());
