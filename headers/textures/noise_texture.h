b40dd6cb443a72d2265d9b4a812091267b6bb9d0
Patrick Wu <magetron1@gmail.com>
2019-06-17 23:56:37 +0100

    add scale feature for noise texture

--- headers/textures/noise_texture.h
+++ headers/textures/noise_texture.h
@@ -7,11 +7,13 @@
 class noise_texture : public texture {
 	public :
 		noise_texture () { }
+		noise_texture (double sc) : scale(sc) { } 
 		virtual vec3 value (double u, double v, const vec3& p) const {
-			return vec3(1.0, 1.0, 1.0) * noise.noise(p);
+			return vec3(1.0, 1.0, 1.0) * noise.noise(scale * p);
 		}
 		
 		perlin noise;
+		double scale;
 };
 
 #endif
