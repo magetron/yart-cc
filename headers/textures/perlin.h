38eb00c9aaa9e48530cb2c33176d2d1b5c3b90e5
Patrick Wu <magetron1@gmail.com>
2019-06-19 13:53:34 +0100

    add new perlin_interp feature

--- headers/textures/perlin.h
+++ headers/textures/perlin.h
@@ -3,12 +3,31 @@
 
 #include "../swap.h"
 
+/* Deprecated
 inline double trilinear_interp (double c[2][2][2], double u, double v, double w) {
 	double accum = 0;
 	for (int i = 0; i < 2; i++)
 		for (int j = 0; j < 2; j++)
 			for (int k = 0; k < 2; k++)
 				accum += (i * u + (1 - i) * (1 - u)) * (j * v + (1 - j) * (1 - v)) *(k * w + (1 - k) * (1 - w)) * c[i][j][k];
+	return accum;
+}
+*/
+
+inline double perlin_interp(vec3 c[2][2][2], double u, double v, double w) {
+	double uu = u * u * (3 - 2 * u);
+	double vv = v * v * (3 - 2 * v);
+	double ww = w * w * (3 - 2 * w);
+	double accum = 0;
+	for (int i = 0; i < 2; i++)
+		for (int j = 0; j < 2; j++)
+			for (int k = 0; k < 2; k++) {
+				vec3 weight_v(u - i, v - j, w - k);
+				accum += 	(i * uu + (1 - i) * (1 - uu)) *
+					    	(j * vv + (1 - j) * (1 - vv)) *
+							(k * ww + (1 - k) * (1 - ww)) *
+							dot(c[i][j][k], weight_v);
+			}
 	return accum;
 }
 
@@ -19,33 +38,29 @@ class perlin {
 			double v = p.y() - floor(p.y());
 			double w = p.z() - floor(p.z());
 			
-			u = (u * u) * (3 - 2 * u);
-			v = (v * v) * (3 - 2 * v);
-			w = (w * w) * (3 - 2 * w);
-
 			int i = floor(p.x());
 			int j = floor(p.y());
 			int k = floor(p.z());
 
-			double c[2][2][2];
+			vec3 c[2][2][2];
 
 			for (int ii = 0; ii < 2; ii++)
 				for (int jj = 0; jj < 2; jj++)
 					for (int kk = 0; kk < 2; kk++)
-						c[ii][jj][kk] = ranfloat[perm_x[(i + ii) & 255] ^ perm_y[(j + jj) & 255] ^ perm_z[(k + kk) & 255]];
+						c[ii][jj][kk] = ranvec[perm_x[(i + ii) & 255] ^ perm_y[(j + jj) & 255] ^ perm_z[(k + kk) & 255]];
 				
-			return trilinear_interp(c, u, v, w);
+			return perlin_interp(c, u, v, w);
 		}
 
-		static double* 	ranfloat;
+		static vec3* ranvec;
 		static int*	perm_x;
 		static int*	perm_y;
 		static int* perm_z;
 };
 
-static double* perlin_generate () {
-	double* p = new double[256]; 
-	for (int i = 0; i < 256; i++) p[i] = drand48();
+static vec3* perlin_generate () {
+	vec3* p = new vec3[256]; 
+	for (int i = 0; i < 256; i++) p[i] = unit_vector(vec3(-1 + 2 * drand48(), -1 + 2 * drand48(), -1 + 2 * drand48()));
 	return p;
 }
 
@@ -63,7 +78,8 @@ static int* perlin_generate_perm () {
 	return p;
 }
 
-double* perlin::ranfloat = perlin_generate();
+vec3* perlin::ranvec = perlin_generate();
+
 int* perlin::perm_x = perlin_generate_perm();
 int* perlin::perm_y = perlin_generate_perm();
 int* perlin::perm_z = perlin_generate_perm();
