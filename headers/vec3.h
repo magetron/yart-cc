2be80266b839b3ba156b8b8658936e1fa5af4c26
Patrick Wu <magetron1@gmail.com>
2019-06-22 21:43:33 +0100

    fix bug for vec3 class

--- headers/vec3.h
+++ headers/vec3.h
@@ -120,6 +120,13 @@ inline vec3& vec3::operator *= (const vec3 &v) {
 	return *this;
 }
 
+inline vec3& vec3::operator *= (const double t) {
+	e[0] *= t;
+	e[1] *= t;
+	e[2] *= t;
+	return *this;
+}
+
 inline vec3& vec3::operator /= (const vec3 &v) {
 	e[0] /= v.e[0];
 	e[1] /= v.e[1];
