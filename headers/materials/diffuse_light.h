90daebbfae4346947a12223af2628aee3ff2d40f
Patrick Wu <magetron1@gmail.com>
2019-07-14 16:17:36 +0100

    add light emitting material

---
+++ headers/materials/diffuse_light.h
@@ -0,0 +1,17 @@
+#ifndef DIFFUSE_LIGHT
+#define DIFFUSE_LIGHT
+
+class diffuse_light : public material {
+	public :
+		diffuse_light (texture *a) : emit(a) { }
+		virtual bool scatter (const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+			return false;
+		}
+		virtual vec3 emitted (double u, double v, const vec3& p) const {
+			return emit -> value(u, v, p);
+		}
+		texture *emit;
+}
+
+
+#endif
