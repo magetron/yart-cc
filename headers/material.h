16ee6684353db99f182a5cd3f187d61f1b314ef5
Patrick Wu <magetron1@gmail.com>
2019-07-14 16:18:34 +0100

    add emit function for material

--- headers/material.h
+++ headers/material.h
@@ -9,6 +9,9 @@ struct hit_record;
 class material {
 	public:
 		virtual bool scatter (const ray& r_in, const hit_record& rec, vec3& attenuation, ray&scattered) const = 0;
+		virtual vec3 emitted (double u, double v, const vec3& p) const {
+			return vec3(0, 0, 0);
+		}	
 };
 
 #endif
