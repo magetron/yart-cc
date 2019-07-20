75ee91cd41c6c685f271c3c1d3caa1884ef7efda
Patrick Wu <magetron1@gmail.com>
2019-07-20 21:14:19 +0100

    add flip_normals to flip rect to make cornell box

---
+++ headers/objects/flip_normals.h
@@ -0,0 +1,23 @@
+#ifndef FLIP_NORMALH
+#define FLIP_NORMALH
+
+#include "../hittable.h"
+
+class flip_normals : public hittable {
+	public:
+		flip_normals (hittable* p) : ptr(p) { }
+		virtual bool hit (const ray& r, double tmin, double tmax, hit_record& rec) const {
+			if (ptr -> hit(r, tmin, tmax, rec)) {
+				rec.normal = - rec.normal;
+				return true;
+			} else return false;
+		}
+
+		virtual bool bounding_box (double t0, double t1, aabb& box) const {
+			return ptr -> bouding_box(t0, t1, box);
+		}
+
+		hittable *ptr;
+};
+
+#endif
