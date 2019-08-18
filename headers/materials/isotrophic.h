070cbc218d4f6b06fd452d9bc3067968696331a3
Patrick Wu <magetron1@gmail.com>
2019-08-18 15:33:56 +0100

    add isotrophic material

---
+++ headers/materials/isotrophic.h
@@ -0,0 +1,17 @@
+#ifndef ISOTROPICH
+#define ISOTROPICH
+
+#include "../material.h"
+
+class isotropic : public material {
+	public:
+		isotropic (texture *a) : albedo(a) { }
+		virtual bool scatter (const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scatterd) const {
+			scatterd = ray(rec.p, random_in_unit_sphere());
+			attenuation = albedo -> value(rec.u, rec.v, rec.p);
+			return true;
+		}
+		texture *albedo;
+};
+
+#endif
