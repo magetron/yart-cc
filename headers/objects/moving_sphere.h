b8ea5d1cedd1f3ba2958dfba36e590712cc789ea
Patrick Wu <magetron1@gmail.com>
2019-08-26 21:21:40 +0100

    add moving sphere for current status

--- headers/objects/moving_sphere.h
+++ headers/objects/moving_sphere.h
@@ -2,6 +2,7 @@
 #define MOVING_SPHEREH
 
 #include "../hittable.h"
+#include "../aabb.h"
 
 class moving_sphere : public hittable {
 	public :
@@ -10,6 +11,7 @@ class moving_sphere : public hittable {
 			centre0(cen0), centre1(cen1), time0(t0), time1(t1), radius(r), mat_ptr(m) { };
 
 		virtual bool hit (const ray& r, double tmin, double tmax, hit_record& rec) const;
+		virtual bool bounding_box (double t0, double t1, aabb& box) const;
 
 		vec3 centre (double time) const;
 		vec3 centre0, centre1;
@@ -20,6 +22,13 @@ class moving_sphere : public hittable {
 
 vec3 moving_sphere::centre (double time) const {
 	return centre0 + ((time - time0) / (time1 - time0)) * (centre1 - centre0);
+}
+
+bool moving_sphere::bounding_box (double t0, double t1, aabb& box) const {
+	aabb box0(centre(t0) - vec3(radius, radius, radius), centre(t0) + vec3(radius, radius, radius));
+	aabb box1(centre(t1) - vec3(radius, radius, radius), centre(t1) + vec3(radius, radius, radius));
+	box = surrounding_box(box0, box1);
+	return true;
 }
 
 bool moving_sphere::hit (const ray& r, double t_min, double t_max, hit_record& rec) const {
